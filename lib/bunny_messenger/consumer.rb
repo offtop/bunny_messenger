b7ff374fd2da7dfe8877d806ed200bf09b796ee5
???????? ???????????? <ivan@srgh.tech>
2021-06-29 18:22:33 +0300

    bump version, failed messages are rejected without requeue

--- lib/bunny_messenger/consumer.rb
+++ lib/bunny_messenger/consumer.rb
@@ -16,7 +16,7 @@ class BunnyMessenger
 
     def perform_on_fail(_delivery_info, _metadata, _payload, _exception); end
 
-    def call
+    def call(multiple=false, requeue=false)
       queue.subscribe(block: true, manual_ack: true) do |delivery_info, metadata, payload|
         begin
           trace_time "Message #{metadata.message_id} processed in" do
@@ -26,7 +26,7 @@ class BunnyMessenger
         rescue StandardError => e
           trace_time "Message #{metadata.message_id} failed with #{e.message}" do
             perform_on_fail(delivery_info, metadata, payload, e)
-            delivery_info.channel.nack(delivery_info.delivery_tag, false, false)
+            delivery_info.channel.nack(delivery_info.delivery_tag, multiple, requeue)
           end
         end
       end
